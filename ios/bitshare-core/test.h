be925fdaa02eeeb9d959813753be50fdae7584c9
koofrank <koofranker@gmail.com>
2018-04-18 18:41:27 +0800

    add boost

---
+++ ios/bitshare-core/test.h
@@ -0,0 +1,9 @@
+//
+//  test.h
+//  cybexMobile
+//
+//  Created by koofrank on 2018/4/18.
+//  Copyright © 2018年 Cybex. All rights reserved.
+//
+
+#pragma once
---
+++ ios/bitshare-core/test.mm
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  cybexMobile
+//
+//  Created by koofrank on 2018/4/18.
+//  Copyright © 2018年 Cybex. All rights reserved.
+//
+
+#include "test.h"
+#include <iostream>
+#include <boost/foreach.hpp>
+
+
